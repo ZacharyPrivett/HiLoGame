a21f3391b740bc8f15dbfb0f250ec6ab576d1b70
ZacharyPrivett <zprivet0@email.cpcc.edu>
2023-02-07 13:06:46 -0500

    update gui still not finished

--- src/HiLoFrame.java
+++ src/HiLoFrame.java
@@ -1,13 +1,26 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class HiLoFrame extends JFrame{
     private JPanel mainPanel;
+    private JTextField guessedNumber;
+    private JButton guessButton;
+    private JLabel guessLabel;
 
+    int winningNumber = (int)(Math.random() * 100 + 1);
     public HiLoFrame(String title) {
        super(title);
 
        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
        this.setContentPane(mainPanel);
        this.pack();
+        guessButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int guess = (int)(Double.parseDouble(guessedNumber.getText()));
+                guessLabel.setText(guess + "number Yea!");
+            }
+        });
     }
 }
